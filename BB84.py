cc386a6dfd393d5f814d2891ed912fb651e5356e
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2018-02-06 17:55:25 +0000

    removing double counts function

--- BB84.py
+++ BB84.py
@@ -4,4 +4,8 @@ Created on Tue Feb  6 13:29:51 2018
 
 @author: fhales
 """
-
+def removeDoublePhotonEvents(data):
+    shiftedPlus = np.append(data[-1], data[:-1])
+    shiftedMinus = np.append(data[1:], data[0])
+    croppedData = data[np.where(np.logical_not(np.logical_and(np.logical_or(np.isclose(data,shiftedPlus),np.isclose(data, shiftedMinus)), np.logical_not(np.isclose(data,5)))))]
+    return croppedData
