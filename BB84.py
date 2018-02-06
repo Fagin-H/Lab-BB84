82abc45ce9345fca32c79471eebb7fc0c7e00d00
Fagin Hales <ucapfh0@ad.ucl.ac.uk>
2018-02-06 15:02:24 +0000

    Removed loop to make more efficent

--- BB84.py
+++ BB84.py
@@ -8,13 +8,8 @@ Created on Tue Feb  6 13:29:51 2018
 import numpy as np
 
 
-data=np.array([[0,1,2,3],[5,5,5,5]])
-data20=[]
-data21=[]
+data=np.array([[0,1,2,3],[5,1,5,5]])
 
-for i in range(len(data[0])):
-    if data[1][i]==5:
-        data20.append(data[0][i])
-        data21.append(data[1][i])
-   
-data2=np.array([data20,data21])     
+index = np.argwhere(data[1]!=5)
+data2 = np.delete(data[0], index)
+print(data2)
