f8132cbf4a7965125b38f3e96c3fd182f41e018d
Oliver Lunt <olunt@lcn.ucl.ac.uk>
2018-02-06 18:32:28 +0000

    Working script to determine basic error rate (photon detected at wrong detector given the voltage step)

--- BB84.py
+++ BB84.py
@@ -5,3 +5,21 @@ Created on Tue Feb  6 13:29:51 2018
 @author: fhales
 """
 
+import numpy as np
+
+def read_data(location):
+    return np.loadtxt(location,delimiter=", ")
+
+def do_stuff():
+    data=np.array([[0,1,2,3,4,5,6],[5,5,5,5,5,5,5]])
+
+    index = np.argwhere(data[1]!=5)
+    data2 = np.delete(data[0], index)
+    diff=np.hstack((data2,[0]))-np.hstack(([0],data2))
+    diff = np.delete(diff, 0)
+    diff=diff[:-1]
+    diff=np.average(diff)
+    print(diff)
+
+if __name__ == "__main__":
+    do_stuff()
--- analysis.py
+++ analysis.py
@@ -7,8 +7,10 @@ pulse_data = data[np.isclose(data[:,1],5)] # Select only the rows which correspo
 voltage_steps = np.transpose(np.array((pulse_data[:,0], np.mod(np.arange(pulse_data.shape[0]), 4)))) # Array for converting timestamp to voltage step
 
 count_data = data[np.logical_not(np.isclose(data[:,1],5))][:,1] - 1
-times_for_counts = data[np.logical_not(np.isclose(data[:,1],5))[0] - 1]
-steps_for_counts = voltage_steps[np.where(np.isclose(voltage_steps[:,0], times_for_counts[:,0]))][:,1]
+times_for_counts = data[np.roll(np.logical_not(np.isclose(data[:,1],5)),-1)]
+sorter = np.argsort(voltage_steps[:,0])
+time_indices = sorter[np.searchsorted(voltage_steps[:,0], times_for_counts[:,0], sorter=sorter)]
+steps_for_counts = voltage_steps[:,1][time_indices]
 
 error_array = np.isclose(count_data, steps_for_counts)
 error_rate = np.mean(error_array)
