599edbfd03c733653444df0396f0344eb3445c54
Fagin Hales <ucapfh0@ad.ucl.ac.uk>
2018-02-06 16:10:36 +0000

    Updated to work with new format

--- BB84.py
+++ BB84.py
@@ -10,12 +10,10 @@ import numpy as np
 def read_data(location):
     return np.loadtxt(location,delimiter=",")
 
-data=np.array([[0,1,2,3,4,5,6],[5,5,5,5,5,5,5]])
+data=read_data('testdata.txt')
 
-index = np.argwhere(data[1]!=5)
-data2 = np.delete(data[0], index)
-diff=np.hstack((data2,[0]))-np.hstack(([0],data2))
-diff = np.delete(diff, 0)
-diff=diff[:-1]
-diff=np.average(diff)
-print(diff)
+index = np.argwhere(data[:,1]!=5.)
+data2 = np.delete(data, index, 0)
+diff=data2[:,0]-np.roll(data2[:,0],1)
+diff=np.average(diff[1:])
+print(diff*81*10**-12)
