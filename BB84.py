64e6c454cb2db6a6da005bcdeec7baaf8f991d5e
Fagin Hales <ucapfh0@ad.ucl.ac.uk>
2018-02-06 16:14:02 +0000

    made average time a function

--- BB84.py
+++ BB84.py
@@ -10,10 +10,13 @@ import numpy as np
 def read_data(location):
     return np.loadtxt(location,delimiter=",")
 
+
+def average_time(data):
+    index = np.argwhere(data[:,1]!=5.)
+    data2 = np.delete(data, index, 0)
+    diff=data2[:,0]-np.roll(data2[:,0],1)
+    diff=np.average(diff[1:])
+    return diff*81*10**-12
+
 data=read_data('testdata.txt')
 
-index = np.argwhere(data[:,1]!=5.)
-data2 = np.delete(data, index, 0)
-diff=data2[:,0]-np.roll(data2[:,0],1)
-diff=np.average(diff[1:])
-print(diff*81*10**-12)
