c807d673049313c9f521c26f35b499a91b1136a0
Fagin Hales <ucapfh0@ad.ucl.ac.uk>
2018-02-06 16:55:10 +0000

    working on stuff

--- BB84.py
+++ BB84.py
@@ -8,15 +8,37 @@ Created on Tue Feb  6 13:29:51 2018
 import numpy as np
 
 def read_data(location):
-    return np.loadtxt(location,delimiter=",")
+    data=np.loadtxt(location,delimiter=",")
+    data[:,0]=data[:,0]*81*10**-12
+    return data
 
-
-def average_time(data):
+def only5(data):
     index = np.argwhere(data[:,1]!=5.)
     data2 = np.delete(data, index, 0)
-    diff=data2[:,0]-np.roll(data2[:,0],1)
+    return data2
+    
+def average_time(data):
+    diff=data[:,0]-np.roll(data[:,0],1)
     diff=np.average(diff[1:])
-    return diff*81*10**-12
+    return diff
 
-data=read_data('testdata.txt')
+
+
+data=only5(read_data('testdata.txt'))
+
+ave=average_time(data)
+
+diff=data[:,0]-np.roll(data[:,0],1)
+index = np.argwhere(diff>1.5*ave)
+
+listindex=[]
+for x in index:
+    listindex.append(x[0])
+
+data2=np.split(data,listindex)
+newdata=data2[0]
+for i in range(len(data2)):
+    newdata=np.hstack(newdata,newdata[-1]+ave)
+    newdata=np.hstack(newdata,data2[i])
+
 
