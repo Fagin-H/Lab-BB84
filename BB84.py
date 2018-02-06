e50c43ec87912a1586b091a9512eefaf693ed371
Fagin Hales <fhales@lcn.ucl.ac.uk>
2018-02-06 13:59:27 +0000

    Started code

--- BB84.py
+++ BB84.py
@@ -5,3 +5,16 @@ Created on Tue Feb  6 13:29:51 2018
 @author: fhales
 """
 
+import numpy as np
+
+
+data=np.array([[0,1,2,3],[5,5,5,5]])
+data20=[]
+data21=[]
+
+for i in range(len(data[0])):
+    if data[1][i]==5:
+        data20.append(data[0][i])
+        data21.append(data[1][i])
+   
+data2=np.array([data20,data21])     
