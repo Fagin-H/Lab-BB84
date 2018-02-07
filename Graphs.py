09f36bd70267b8282798c4be14e0372f399e71c0
Fagin Hales <ucapfh0@ad.ucl.ac.uk>
2018-02-07 12:49:13 +0000

    started graphs

--- Graphs.py
+++ Graphs.py
@@ -5,3 +5,17 @@ Created on Wed Feb  7 12:17:35 2018
 @author: ucapfh0
 """
 
+import numpy as np
+
+
+#data=np.loadtxt('data0.txt',delimiter=" ")
+
+data0=data[0::4][:,1]
+data1=data[1::4][:,1]
+data2=data[2::4][:,1]
+data3=data[3::4][:,1]
+
+for x in data0:
+    if x == 1:
+        
+
