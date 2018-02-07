Fixed BB84 error rate calculation

BB84.py
@@ -2,24 +2,76 @@
 """
 Created on Tue Feb  6 13:29:51 2018
 
-@author: fhales
+@author: fhales and cderbz
 """
 
 import numpy as np
 
 def read_data(location):
-    return np.loadtxt(location,delimiter=", ")
+    data=np.loadtxt(location,delimiter=",")
+    #data[:,0]=data[:,0]*81*10**-12
+    return data
 
-def do_stuff():
-    data=np.array([[0,1,2,3,4,5,6],[5,5,5,5,5,5,5]])
+def only5(data):
+    indexno5 = np.argwhere(data[:,1]!=5.)
+    index5 = np.argwhere(data[:,1]==5.)
+    data2 = np.delete(data, indexno5, 0)
+    return data2, index5
+    
+def average_time(only5_data):
+    diff=only5_data[:,0]-np.roll(only5_data[:,0],1)
+    diff=np.average(diff[1:])
+    return diff
 
-    index = np.argwhere(data[1]!=5)
-    data2 = np.delete(data[0], index)
-    diff=np.hstack((data2,[0]))-np.hstack(([0],data2))
-    diff = np.delete(diff, 0)
-    diff=diff[:-1]
-    diff=np.average(diff)
-    print(diff)
+def splitdata(data):
+    only5_data,index5 = only5(data)
+    ave=average_time(only5_data)
+    diff=only5_data[:,0]-np.roll(only5_data[:,0],1)
+    index = np.argwhere(diff>1.5*ave)
+    listindex=index.flatten()
+    split_index=np.take(index5,listindex)    
+    split_index=split_index.flatten()
+    
+    
+    split_data=np.split(data,split_index)
+    return split_data
 
+def removeDoublePhotonEvents(data):
+    shiftedPlus = np.append(data[-1:], data[:-1], axis = 0)
+    shiftedMinus = np.append(data[0:], data[:0],axis = 0)
+    croppedData = data[np.where(np.logical_not(np.logical_and(np.logical_or(np.isclose(data[:,1],shiftedPlus[:,1]),np.isclose(data[:,1], shiftedMinus[:,1])), np.logical_not(np.isclose(data[:,1],5)))))]
+    return croppedData
+ 
+def fixdata(data):
+    data2=splitdata(data)#removeDoublePhotonEvents(data))
+    return data2
+
+#testdata=read_data('testdata.txt')
+    
 if __name__ == "__main__":
-    do_stuff()
+    
+    filename = "4level_200bins_2s_123width"
+    data = read_data(filename + ".txt")
+    data = splitdata(data)
+    for index, array in np.ndenumerate(data):
+        np.savetxt(filename + "_" + str(index) + ".txt", array, delimiter = ",")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

analysis.py
@@ -25,10 +25,16 @@ def BB84_error_rate(count_data, steps_for_counts):
     count_data = count_data.astype(int)
     count_data = count_data.reshape(count_data.size,1)
     steps_for_counts = steps_for_counts.astype(int)
-
-    valid_detectors = np.array([[0,2,3],[1,2,3],[0,1,2],[0,1,3]])
-    error_array = np.isclose(valid_detectors[steps_for_counts], count_data)
-    error_list = np.logical_not(np.any(error_array, axis=1))
+    
+    detectors_for_basis = np.array([[0,1],[0,1],[2,3],[2,3]])
+    detection_array = np.isclose(detectors_for_basis[steps_for_counts], count_data)
+    detection_list = np.any(detection_array, axis=1)
+    counts_to_consider = count_data[detection_list]
+    steps_to_consider = steps_for_counts[detection_list]
+    
+    counts_to_consider = counts_to_consider.reshape(counts_to_consider.size)
+    
+    error_list = np.logical_not(np.isclose(counts_to_consider, steps_to_consider))
     error_rate = np.mean(error_list)
 
     print("BB84 error rate is {}".format(error_rate))
@@ -37,4 +43,4 @@ if __name__ == "__main__":
 
     data = read_data("countdata.txt")
     count_data, steps_for_counts = get_counts_with_voltage_steps(data)
-    basic_error_rate(count_data, steps_for_counts)
+    BB84_error_rate(count_data, steps_for_counts)