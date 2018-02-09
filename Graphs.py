42a09fd34e13e1ee1c5dd4e5a16c2ebea2cceeaa
Fagin Hales <fhales@lcn.ucl.ac.uk>
2018-02-09 16:01:51 +0000

    normaised graphs

--- Graphs.py
+++ Graphs.py
@@ -8,6 +8,7 @@ Created on Wed Feb  7 12:17:35 2018
 import numpy as np
 import matplotlib.pyplot as plt
 import BB84
+import collections as col
 
 
 def no5(data):
@@ -19,17 +20,26 @@ def group_data(data,n):
     index5=index5[:-1]
     return no5(np.take(data[:,1],index5[n::4]+1))
 
+def make_hist(data):
+    dic=col.Counter(data)
+    return (dic[1]/(dic[1]+dic[2]),dic[2]/(dic[1]+dic[2]),dic[3]/(dic[3]+dic[4]),dic[4]/(dic[3]+dic[4]))
+
+
+
 def save_figs(data,location):
     k=0
     data_fixed=BB84.fixdata(data)
     for data_n in data_fixed:
         k+=1
         for i in range(4):
-            graph_data=group_data(data_n,i)
-            plt.hist(graph_data)
-            plt.savefig(location+'/'+str(k)+str(i)+'.png')
+            hist_data=make_hist(group_data(data_n,i))
+            objects = np.arange(4)+1
+            y_pos = np.arange(len(objects))             
+            plt.bar(y_pos, hist_data, align='center', alpha=0.5)
+            plt.xticks(y_pos, objects)
+            plt.savefig(location+'/'+str(k)+str(i+1)+'.png')
             plt.close()
 
 if __name__=='__main__':
-#    data=np.loadtxt('BB84/countdata.txt',delimiter=",")
+    data=np.loadtxt('data40.txt',delimiter=" ")
     save_figs(data,'real_data_bar_plots')
